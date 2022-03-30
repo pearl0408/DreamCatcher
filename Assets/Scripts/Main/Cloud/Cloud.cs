f43b423de0f6a1a2e0c8363165225ebe7842c53c
pearl0408 <roskfl000408@naver.com>
2022-03-30 20:27:57 +0900

    구름 사라지는 기능 수정
    -렌더러가 카메라를 벗어나면 스택에 push에서 x좌표가 일정 범위가 넘으면 push로 수정

--- Assets/Scripts/Main/Cloud/Cloud.cs
+++ Assets/Scripts/Main/Cloud/Cloud.cs
@@ -6,7 +6,8 @@ public class Cloud : Poolable
 {
     //Pooling을 할 오브젝트 Cloud에 추가하는 컴포넌트
 
-    float speed = 100f;
+    private float speed = 100f;     //구름 속도
+    private int range_x = 1500;
     Vector2 dir;
 
     private void Update()
@@ -14,6 +15,11 @@ public class Cloud : Poolable
         //매 프레임마다 구름 이동
 
         transform.Translate(dir * speed * Time.deltaTime);
+
+        if (transform.position.x > range_x)
+        {
+            Push();
+        }
     }
 
     private void OnEnable()
@@ -22,13 +28,16 @@ public class Cloud : Poolable
 
         //벡터 방향 설정
         transform.position = Vector2.zero;
-        dir = new Vector2(100f, 0).normalized;
+        dir = new Vector2(50f, 0).normalized;
     }
 
+    /* 
+    //화면마다 push 되는 지점이 달라서 없애거나 수정
     private void OnBecameInvisible()
     {
         //카메라에서 렌더러가 표시되지 않으면 다시 풀에 넣는 함수
 
         Push();
     }
+    */
 }
