c9f60e5eb52383f08bdaa24932201bac7a011c7d
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2022-05-25 00:42:34 +0900

    제작창 1차 구성

---
+++ Assets/Scripts/Making/MakingUIManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakingUIManager : MonoBehaviour
+{
+    public GameObject StringWindow, FeatherWindow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StringWin()
+    {
+        Debug.Log("StringWin");
+    }
+
+    public void FeatherWin()
+    {
+        Debug.Log("FeatherWin");
+    }
+}
